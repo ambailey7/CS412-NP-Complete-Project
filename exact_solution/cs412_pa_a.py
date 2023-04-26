71965335126122b32c0dcaf57cfe68d6f915abbf
Andrew Bailey <baile6am@dukes.jmu.edu>
2023-04-26 15:11:27 -0400

    exact solution

--- exact_solution/cs412_pa_a.py
+++ exact_solution/cs412_pa_a.py
@@ -14,22 +14,49 @@ from itertools import permutations
 
 
 # matrix W, start at s
-def exactTSP(W, s):
-    perms = []
-    nums = [n for n in range(len(W))]
-    p = permutations(nums)
-    for i in p:
-        print(i)
-
-
+def exactTSP(edges, s):
+    vertices = []
+    for u, _ in edges:
+        if u not in vertices:
+            vertices.append(u)
+    
+    min = float('inf')
+    # create permutations that represent paths in our graph
+    perms = permutations(vertices)
+    path = []
+    for next in perms:
+        if next[0] == s: # only start with s
+            cycle = list(next)
+            cycle.append(s) # append starting vertex to create a cycle
+            weight = 0
+            path = [s] 
+            # construct the path/weight for the permutation
+            for i in range(len(cycle) - 1):
+                u, v = (cycle[i], cycle[i + 1])
+                if (u, v) in edges:
+                    weight += edges[(u, v)]
+                    path.append(v)
+            if weight < min:
+                min = weight
+                shortest_path = path
+    # print output
+    print(min)
+    print(' '.join(shortest_path))
 
 
 def main():
     # input is a number n and an nXn matrix
-    n = int(input())
-    wMatrix = [[int(x) for x in input().split()] for _ in range(n)]
+    n, e = [int(x) for x in input().split()]
+    edges = dict()
+    for _ in range(e):
+        u, v, w = input().split()
+        w = int(w)
+        edges[(u, v)] = w
+        edges[(v, u)] = w   
+    print(edges)
+
     print()
-    print(exactTSP(wMatrix, 0)) 
+    exactTSP(edges, "a") 
     pass
 
 if __name__ == "__main__":
